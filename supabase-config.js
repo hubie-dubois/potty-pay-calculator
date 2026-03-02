5660e170c63ee83a233cf94dd18495130e7c7716
hubie-dubois <chrisfiorillo2@gmail.com>
2026-03-01 20:31:15 -0600

    Configure Supabase URL and publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.POTTY_PAY_SUPABASE_URL = "";
-window.POTTY_PAY_SUPABASE_ANON_KEY = "";
+window.POTTY_PAY_SUPABASE_URL = "https://qqykkrplwfueiiagvqvh.supabase.co";
+window.POTTY_PAY_SUPABASE_ANON_KEY = "sb_publishable_LsojxjE-8AgP8EkmJYOLHg_ABgjaqtJ";
